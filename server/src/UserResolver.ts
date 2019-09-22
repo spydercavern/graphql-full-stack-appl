6dd47027bc5162f1f9871c654b597b745ccf0a14
suman.singh@verizonmedia.com <suman.singh@verizonmedia.com>
2019-09-22 23:30:30 +0530

    Basic JWT integration

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,23 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+  Ctx
+} from "type-graphql";
 import { User } from "./entity/User";
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { MyContext } from "./ApolloContext";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: String;
+}
 
 @Resolver()
 export class UserResolver {
@@ -30,5 +47,42 @@ export class UserResolver {
       return false;
     }
     return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw Error("Invalid user/User does not exist");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw Error("Invalid user/password");
+    }
+    const { res } = ctx;
+    // refresh token in cookie
+    res.cookie(
+      "jid",
+      sign({ userId: user.id }, "al;kas;dklf;", {
+        expiresIn: "7d"
+      }),
+      {
+        httpOnly: true
+      }
+    );
+
+    return {
+      accessToken: sign(
+        { userId: user.id, userEmail: user.email },
+        "asdlfajs;dlfka",
+        { expiresIn: "15m" }
+      )
+    };
   }
 }
