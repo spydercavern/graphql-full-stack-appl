dfdb9204ce658582048c0811de465c29b5f204b5
suman.singh@verizonmedia.com <suman.singh@verizonmedia.com>
2019-09-22 22:43:31 +0530

    Basic graphql mutation with user registration

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,9 +1,29 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { User } from "./entity/User";
+import { hash } from "bcryptjs";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "Hey there from user resovler";
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    try {
+      const hashedPwd = await hash(password, 12);
+      await User.insert({
+        email,
+        password: hashedPwd
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,16 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { UserResolver } from "./UserResolver";
 import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 
 (async () => {
   const app = express();
   app.get("/", ({ res }) => {
     res!.send("Helo");
   });
+
+  // Create a connction for type orm, reads config from ormconfig.json file in the root
+  createConnection();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
