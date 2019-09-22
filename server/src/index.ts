9fabaafdb9c0826206471c1003ab0a8ee6f223e3
suman.singh@verizonmedia.com <suman.singh@verizonmedia.com>
2019-09-22 21:54:32 +0530

    Basic Apollo Graphql server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 (async () => {
   const app = express();
   app.get("/", ({ res }) => {
     res!.send("Helo");
   });
+
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Howdy there!"
+      }
+    }
+  });
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log(`express is up running at port 4000`);
   });
